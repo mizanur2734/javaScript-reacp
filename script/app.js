459a32890bd80ee7812572803574ec8765db9f78
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-20 21:28:03 +0600

    variable declaration

---
+++ script/app.js
@@ -0,0 +1,21 @@
+// variable declaration
+// let and const  
+
+// let name = "Johan";
+// name = "Johan Doe";
+// console.log(name);
+
+// const age = 30;
+// age = 31; // This will throw an error because age is a constant
+// console.log(age);
+
+// num = 40;
+// if(num >= 20 || num <= 30){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+const name = ["Johan", "Doe", "Smith"];
+
+console.log(name.length);
