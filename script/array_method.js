69e549c6f266cda4a9b986309f66caed223c373d
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-20 22:34:36 +0600

    added the array-method

---
+++ script/array_method.js
@@ -0,0 +1,17 @@
+const products = [
+  { name: "laptop", price: 32000, brand: "lenovo", color: "silver" },
+  { name: "phone", price: 7000, brand: "iphone", color: "golden" },
+  { name: "watch", price: 3000, brand: "casio", color: "black" },
+  { name: "sunglass", price: 300, brand: "ribon", color: "yellow" },
+  { name: "camera", price: 9000, brand: "canon", color: "gray" },
+];
+
+// const brands = products.map(product => product.brand);
+// console.log(brands)
+
+// products.forEach(product => console.log(product.color))
+
+// const result = products.filter(product => product.price < 10000);
+
+const result = products.find((product) => product.price < 10000);
+console.log(result);
