ebe17ab809adc531a155d52a3ac2ba1e21dcba78
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-20 22:12:13 +0600

    added templated

--- script/app.js
+++ script/app.js
@@ -16,6 +16,31 @@
 //     console.log("false")
 // }
 
-const name = ["Johan", "Doe", "Smith"];
+// const name = ["Johan", "Doe", "Smith"];
+// name.push("Johnson");
+// name.pop()
+// name.unshift("Mr.");
+// name.shift();
+// console.log(name.slice(0, 2));
 
-console.log(name.length);
+// for (let index = 0; index < name.length; index++) {
+//     const element = name[index];
+//     console.log(element)
+// }
+
+// function sum (a, b){
+//     const result = a + b;
+//     return result
+// }
+// const output = sum(5, 10)
+// console.log(output)
+
+// const friends = ["Johan", "Doe", "Smith"];
+// const person = {
+//     name: "Johan",
+//     age: 30,
+//     name: friends,
+//     city: "New York"
+// }
+
+// console.log(person.name[1]) // Logs the friends array
---
+++ script/templet.js
@@ -0,0 +1,15 @@
+// template String 
+
+// const firstName = 'John';
+// const lastname = 'Doe';
+// console.log(`My name is ${firstName} ${lastname}`)
+
+// const sum = (a, b) => {
+//     return a + b;
+// }
+// const output = sum(5, 10);
+// console.log(`The sum of 5 and 10 is ${output}`)
+
+// const numbers = [1, 2, 3, 4, 5];
+// const newArry = [...numbers, 6, 7, 8];
+// console.log(newArry)
