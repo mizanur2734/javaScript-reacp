8893f26493a7c1f0ac62fbd15ff8024ed4d8f2a8
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-24 23:44:29 +0600

    added the dot notation

---
+++ script/notation.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "John Doe",
+    age: 30,
+    city: "New York",
+    friends: ['Tom', 'Jerry', 'Mickey', 'Donald'],
+    10: "ten"
+}
+
+const name = person["name"];
+const ten = person[10];
+console.log(name)
+console.log(ten)
